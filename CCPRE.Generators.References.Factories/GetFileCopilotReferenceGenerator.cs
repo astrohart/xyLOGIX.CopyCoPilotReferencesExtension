a70a0d10cf85653b56484a4fdbe7fdae291d85f2
Brian Hart <brian.hart@xylogix.net>
2025-11-16 13:22:21 -0700

    Create GetFileCopilotReferenceGenerator.cs
    - Introduce a new static class `GetFileCopilotReferenceGenerator`
      in the `CCPRE.Generators.References.Factories` namespace to
      provide singleton-like access to an `ICopilotReferenceGenerator`
      instance.
    - Add the following namespaces to support the implementation:
      - `CCPRE.Generators.References.Interfaces`
      - `PostSharp.Patterns.Diagnostics`
      - `System`
      - `System.Diagnostics`
      - `xyLOGIX.Core.Debug`
    - Define a static constructor `[Log(AttributeExclude = true)]`
      to ensure one-time initialization of static data.
    - Add a public static method `SoleInstance()` to:
      - Return the sole instance of the `ICopilotReferenceGenerator`
        interface using `FileCopilotReferenceGenerator.Instance`.
      - Handle exceptions by logging them with `DebugUtils.LogException(ex)`
        and returning a default value in case of errors.
      - Use `[DebuggerStepThrough]` to skip the method during debugging
        and `[return: NotLogged]` to exclude the return value from logging.
    - Ensure thread safety, centralized access, and improved diagnostics
      with logging and debugging utilities.

---
+++ CCPRE.Generators.References.Factories/GetFileCopilotReferenceGenerator.cs
@@ -0,0 +1,61 @@
+﻿using CCPRE.Generators.References.Interfaces;
+using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+
+namespace CCPRE.Generators.References.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:CCPRE.Generators.References.Interfaces.ICopilotReferenceGenerator" />
+    /// interface.
+    /// </summary>
+    public static class GetFileCopilotReferenceGenerator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Factories.GetFileCopilotReferenceGenerator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetFileCopilotReferenceGenerator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:CCPRE.Generators.References.Interfaces.ICopilotReferenceGenerator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:CCPRE.Generators.References.Interfaces.ICopilotReferenceGenerator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static ICopilotReferenceGenerator SoleInstance()
+        {
+            ICopilotReferenceGenerator result;
+
+            try
+            {
+                result = FileCopilotReferenceGenerator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
