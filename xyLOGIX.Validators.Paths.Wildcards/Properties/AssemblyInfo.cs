e6a2791daf20e250f50c1e52e45c06777c26aa00
Brian Hart <brian.hart@xylogix.net>
2025-11-16 12:32:56 -0700

    Latest updates on 11/16/2025 at 12:32 PM * At 12:32 PM  on 11/16/2025, the Project File Renamer was invoked, and is about to replace all occurrences of 'xyLOGIX.Validators.Paths.Wildcards' with 'CCPRE.Generators.References.Validators' in the files, folders, and names of folders of the 'C:\Users\Brian Hart\source\repos\astrohart\xyLOGIX.CopyCoPilotReferencesExtension' folder.
    --
    Produced by xyLOGIX Project File Renamer.  Copyright ?? 2019-24 by xyLOGIX, LLC.  All rights reserved.

---
+++ xyLOGIX.Validators.Paths.Wildcards/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.Validators.Paths.Wildcards")]
+[assembly: AssemblyDescription("Provides object(s) and functionality for generating GitHub Copilot Chat reference(s) from Visual Studio selection(s) in Solution Explorer.  Supports multiple reference format(s) including file path(s), project(s), and code element(s) (class(es), method(s), property(ies), field(s), event(s), and constant(s)).  Implements the Singleton Strategy Factory pattern to dynamically select the appropriate reference generation strategy based on the type of Visual Studio element selected by the user.  This particular library holds the concrete class(es) and implementation(s) of the component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Copy Copilot Reference Extension")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2025 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Copy Copilot Reference Extension.  Copy Copilot Reference Extension is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.  Other brand names, brands, logos, service marks, trademarks, and registered trademarks herein belong to their respective owners.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d2555d9-a00b-48d7-9c0d-d174088b22cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
