93d9a0e607ea59f4812acd1bf70c9ebc1a89b2ac
Brian Hart <brian.hart@xylogix.net>
2025-11-16 16:44:05 -0700

    Introduce Copy Copilot References extension
    - Add auto-generated files `Commands.cs` and `source.extension.cs`
      to define constants, GUIDs, and metadata for the extension.
    - Implement `CopyCoPilotReferencesCommand` to copy selected file
      references from Solution Explorer to the clipboard in GitHub
      Copilot reference format.
      - Include methods for command execution, clipboard operations,
        path normalization, and logging.
    - Introduce `CopyCoPilotReferencesPackage` to register the extension
      and initialize the command asynchronously.
      - Support background loading to improve Visual Studio startup
        performance.
    - Update `.vsct` file to integrate the command into Visual Studio's
      menu system under the Solution Explorer context menu.
    - Update project file (`.csproj`) to include new source files,
      resources, and build actions for `.vsct` and `.vsixmanifest`.
    - Enhance assembly metadata in `AssemblyInfo.cs` to reflect the new
      product name, description, and version (`2.0.0.0`).
    - Add branding resources (`Favicon.ico`, `Purcell-Icon.png`,
      `Reference.png`) for the extension's UI.
    - Update `README.md` with detailed documentation of the new classes
      and their functionality.
    - Define installation targets and dependencies in
      `source.extension.vsixmanifest` for Visual Studio versions 17.0 to
      18.0 and `.NET Framework 4.7.2`.
    - Improve logging and debugging with utilities for capturing debug
      information and exceptions.
    - Set extension version to `1.1` and add author information
      (`Eddie Green`) in metadata files.

---
+++ CopyCoPilotReferencesExtension/CopyCoPilotReferencesPackage.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.Shell;
+using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyCoPilotReferencesExtension
+{
+    /// <summary>
+    /// Represents the Visual Studio package for the <c>CopyCoPilotReferences</c>
+    /// extension.
+    /// </summary>
+    /// <remarks>
+    /// This package is registered with Visual Studio to provide the functionality of
+    /// the
+    /// <c>CopyCoPilotReferences</c> extension.
+    /// <para />
+    /// It supports asynchronous initialization and background loading to improve
+    /// performance during Visual Studio startup.
+    /// </remarks>
+    [PackageRegistration(
+         UseManagedResourcesOnly = true, AllowsBackgroundLoading = true
+     ), Guid(PackageGuidString), ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class CopyCoPilotReferencesPackage : AsyncPackage
+    {
+        /// <summary>
+        /// The <see cref="T:System.Guid" /> string for the package.
+        /// </summary>
+        public const string PackageGuidString =
+            "f5e53b6a-4f5f-4f87-b6d9-167b162f3b8b";
+
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:CopyCoPilotReferencesExtension.CopyCoPilotReferencesPackage" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static CopyCoPilotReferencesPackage() { }
+
+        /// <summary>
+        /// Creates a new instance of
+        /// <see cref="T:CopyCoPilotReferencesExtension.CopyCoPilotReferencesPackage" />
+        /// and returns a reference to it.
+        /// </summary>
+        /// <remarks>
+        /// We've decorated this constructor with the
+        /// <c>[Log(AttributeExclude = true)]</c> attribute in order to simplify the
+        /// logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        public CopyCoPilotReferencesPackage() { }
+
+        /// <summary>
+        /// The async initialization portion of the package initialization process. This
+        /// method is invoked from a background thread.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// A cancellation token to monitor for
+        /// initialization cancellation, which can occur when VS is shutting down.
+        /// </param>
+        /// <param name="progress">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:System.IProgress`1" /> interface that iṡ provided with a reference
+        /// to <see cref="T:Microsoft.VisualStudio.Shell.ServiceProgressData" /> as its
+        /// type parameter.
+        /// <para />
+        /// This object is used to report progress during initialization.
+        /// </param>
+        /// <returns>
+        /// A task representing the async work of package initialization, or an
+        /// already completed task if there is none. Do not return null from this method.
+        /// </returns>
+        protected override async Task InitializeAsync(
+            CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress
+        )
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(
+                cancellationToken
+            );
+            await CopyCoPilotReferencesCommand.InitializeAsync(this);
+        }
+    }
+}
--- CopyCoPilotReferencesExtension/Properties/AssemblyInfo.cs
+++ CopyCoPilotReferencesExtension/Properties/AssemblyInfo.cs
@@ -1,39 +1,32 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CopyCoPilotReferencesExtension")]
-[assembly: AssemblyDescription("Provides objects and functionality for ")]
+[assembly: AssemblyDescription("VSIX for Visual Studio 2022 that copies selected Solution Explorer files as GitHub Copilot Chat #file:'<path>' references (solution-relative, multi-select).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xyLOGIX, LLC")]
-[assembly: AssemblyProduct("xyLOGIX Support Library for CopyCoPilotReferencesExtension")]
-[assembly:
-    AssemblyCopyright(
-        "Copyright © 2021-25 by xyLOGIX, LLC.  All rights reserved.")]
-[assembly:
-    AssemblyTrademark(
-        "This software is a component of xyLOGIX Foo.  Foo is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.  Other brand names, brands, logos, service marks, trademarks, and registered trademarks herein belong to their respective owners.")]
+[assembly: AssemblyProduct("xyLOGIX Copy Copilot References Extension")]
+[assembly: AssemblyCopyright("Copyright © 2025 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly: AssemblyTrademark("This software component and its name bear no trademarks, in accordance with the GNU General Public License, version 3.")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5b82376-9141-440a-b146-8deea070ac30")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
