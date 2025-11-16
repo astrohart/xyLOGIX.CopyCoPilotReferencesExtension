c3e2a494de0da15136f1982bb3cb846d1ee25ec0
Brian Hart <brian.hart@xylogix.net>
2025-11-16 12:41:12 -0700

    Create GetCopilotReferenceGeneratorTypeValidator.cs
    - Add a new source file `GetCopilotReferenceGeneratorTypeValidator.cs`
      to the project `CCPRE.Generators.References.Validators.Factories.csproj`
      by including it in the `<Compile>` section of the project file.
    - Introduce a static class `GetCopilotReferenceGeneratorTypeValidator`
      to provide a singleton-like access pattern for the
      `ICopilotReferenceGeneratorTypeValidator` interface.
    - Decorate the static constructor with `[Log(AttributeExclude = true)]`
      to exclude logging for the constructor.
    - Add a static method `SoleInstance()` to:
      - Return the sole instance of the `ICopilotReferenceGeneratorTypeValidator` interface.
      - Handle exceptions during instance retrieval using a `try-catch` block
        and log exceptions with `DebugUtils.LogException(ex)`.
      - Return `default` in case of an exception.
    - Include the following namespaces:
      - `CCPRE.Generators.References.Validators.Interfaces`
      - `PostSharp.Patterns.Diagnostics`
      - `System`
      - `System.Diagnostics`
      - `xyLOGIX.Core.Debug`
    - Decorate `SoleInstance()` with `[DebuggerStepThrough]` to optimize debugging
      and `[return: NotLogged]` to exclude the return value from logging.

---
+++ CCPRE.Generators.References.Validators.Factories/GetCopilotReferenceGeneratorTypeValidator.cs
@@ -0,0 +1,64 @@
+﻿using CCPRE.Generators.References.Validators.Interfaces;
+using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+
+namespace CCPRE.Generators.References.Validators.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see
+    ///     cref="T:CCPRE.Generators.References.Validators.Interfaces.ICopilotReferenceGeneratorTypeValidator" />
+    /// interface.
+    /// </summary>
+    public static class GetCopilotReferenceGeneratorTypeValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Validators.Factories.GetCopilotReferenceGeneratorTypeValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetCopilotReferenceGeneratorTypeValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Validators.Interfaces.ICopilotReferenceGeneratorTypeValidator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Validators.Interfaces.ICopilotReferenceGeneratorTypeValidator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static ICopilotReferenceGeneratorTypeValidator SoleInstance()
+        {
+            ICopilotReferenceGeneratorTypeValidator result;
+
+            try
+            {
+                result = CopilotReferenceGeneratorTypeValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
