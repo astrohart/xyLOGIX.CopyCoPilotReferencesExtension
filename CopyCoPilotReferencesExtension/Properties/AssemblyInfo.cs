4ee38bfcadf6f45d7bbbc87434c6be98999e3db3
Brian Hart <brian.hart@xylogix.net>
2025-11-16 11:25:24 -0700

    Update AssemblyInfo.cs
    - Add `using` directives for `System.Reflection` and `System.Runtime.InteropServices` to include namespaces for assembly metadata and COM visibility.
    - Introduce assembly-level attributes to define metadata:
      - `AssemblyTitle`: Set to "CopyCoPilotReferencesExtension".
      - `AssemblyDescription`: Describe the assembly as a Visual Studio 2022 extension for copying Solution Explorer files as GitHub Copilot Chat references.
      - `AssemblyConfiguration`: Leave empty for build configuration.
      - `AssemblyCompany`: Set to "xyLOGIX, LLC".
      - `AssemblyProduct`: Set to "xyLOGIX Copy Copilot References Extension".
      - `AssemblyCopyright`: Include copyright for 2025.
      - `AssemblyTrademark`: Indicate no trademarks, per GNU GPL v3.
      - `AssemblyCulture`: Leave empty for localization.
    - Set `ComVisible` to `false` to hide types from COM components.
    - Define versioning information:
      - `AssemblyVersion`: Set to "2.0.0.0".
      - `AssemblyFileVersion`: Set to "2.0.0.0".

---
+++ CopyCoPilotReferencesExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CopyCoPilotReferencesExtension")]
+[assembly: AssemblyDescription("VSIX for Visual Studio 2022 that copies selected Solution Explorer files as GitHub Copilot Chat #file:'<path>' references (solution-relative, multi-select).")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Copy Copilot References Extension")]
+[assembly: AssemblyCopyright("Copyright © 2025 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly: AssemblyTrademark("This software component and its name bear no trademarks, in accordance with the GNU General Public License, version 3.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
