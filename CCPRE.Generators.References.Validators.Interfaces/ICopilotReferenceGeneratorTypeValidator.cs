1764e46cffd4af36c3bf2622ce00209b97c39c7f
Brian Hart <brian.hart@xylogix.net>
2025-11-16 12:38:46 -0700

    Create ICopilotReferenceGeneratorTypeValidator.cs
    - Add a new file `ICopilotReferenceGeneratorTypeValidator.cs` to the
      project `CCPRE.Generators.References.Validators.Interfaces.csproj`
      by including it in the `<Compile>` section of the project file.
    - Define a new interface `ICopilotReferenceGeneratorTypeValidator`
      to validate `CopilotReferenceGeneratorType` enumeration values.
      - Include a method `IsValid` that checks if a given
        `CopilotReferenceGeneratorType` value is valid.
      - Document the interface and its method with XML comments.
    - Add a `using` directive for the
      `CCPRE.Generators.References.Constants` namespace to reference
      the `CopilotReferenceGeneratorType` enumeration.

---
+++ CCPRE.Generators.References.Validators.Interfaces/ICopilotReferenceGeneratorTypeValidator.cs
@@ -0,0 +1,40 @@
+﻿using CCPRE.Generators.References.Constants;
+
+namespace CCPRE.Generators.References.Validators.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of a validator of
+    /// <see
+    ///     cref="T:CCPRE.Generators.References.Constants.CopilotReferenceGeneratorType" />
+    /// enumeration values.
+    /// </summary>
+    /// <remarks>
+    /// Specifically, objects that implement this interface ascertain whether the
+    /// values of variables fall within the value set that is defined by the
+    /// <see
+    ///     cref="T:CCPRE.Generators.References.Constants.CopilotReferenceGeneratorType" />
+    /// enumeration.
+    /// </remarks>
+    public interface ICopilotReferenceGeneratorTypeValidator
+    {
+        /// <summary>
+        /// Determines whether the copilot reference generator <paramref name="type" />
+        /// value passed is within the value set that is defined by the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Constants.CopilotReferenceGeneratorType" />
+        /// enumeration.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) One of the
+        /// <see
+        ///     cref="T:CCPRE.Generators.References.Constants.CopilotReferenceGeneratorType" />
+        /// values that is to be examined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the copilot reference generator
+        /// <paramref name="type" /> falls within the defined value set;
+        /// <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid(CopilotReferenceGeneratorType type);
+    }
+}
